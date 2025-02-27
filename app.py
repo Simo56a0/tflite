90470664ad6c8a03a6fd0b640cdb212cbe6df0f3
Simo56a0 <159005794+Simo56a0@users.noreply.github.com>
2025-02-27 18:14:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,32 +8,27 @@ app = Flask(__name__)
 interpreter = tf.lite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
 
-# Function to make predictions
 def predict(input_data):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    # Preprocess input data
     input_data = np.array(input_data, dtype=np.float32)
     interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
 
-    # Get the output
     output_data = interpreter.get_tensor(output_details[0]['index'])
     return output_data.tolist()
 
 @app.route('/predict', methods=['POST'])
 def predict_route():
     try:
-        # Get input data from request
         data = request.get_json(force=True)
-        input_data = data['input']  # The input should be passed as a list or array
+        input_data = data['input']
         predictions = predict(input_data)
-
         return jsonify({"predictions": predictions})
     
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
